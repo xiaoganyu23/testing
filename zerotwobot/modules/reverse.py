daa73f14e9eb27019dec78f3a9e28d529f961aa0
Kirito <96290899+kirito-1240@users.noreply.github.com>
2022-05-01 20:26:42 +0530

    Update reverse.py

--- zerotwobot/modules/reverse.py
+++ zerotwobot/modules/reverse.py
@@ -72,7 +72,10 @@ def reverse(update: Update, context: CallbackContext):
         return
     else:
         message.reply_text("Command should be used with replying to an image or url should given.")
-REVERSE_HANDLER = DisableAbleCommandHandler( ["reverse", "pp", "r"], reverse, run_async=True)
+        
+REVERSE_HANDLER = DisableAbleCommandHandler( ["reverse", "pp", "grs", "r"], reverse, run_async=True)
+dispatcher.add_handler(REVERSE_HANDLER)
+
 __help__ = """
 Reverse search any image using google image search.
 
