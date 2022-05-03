a7c6a2349572c0305495f133bbaf8ca4f8c99196
Kanekiken099999 <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-03 20:06:11 +0530

    Update alive.py

--- zerotwobot/modules/alive.py
+++ zerotwobot/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Naruto Uzumaki.** \n\n"
+  TEXT += "❍ **I'm Working Properly** \n\n"
+  TEXT += f"❍ **My Master : [Rin Okumura](https://t.me/Redeye_Ghoul)** \n\n"
+  TEXT += f"❍ **Library Version :** `{telever}` \n\n"
+  TEXT += f"❍ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"❍ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/EmiexRobot?start=help"), Button.url("Support", "https://t.me/emikosupport")]]
+  BUTTON = [[Button.url("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ✉️", "https://t.me/naruto_robot_support"), Button.url("𝙽𝙴𝚃𝚆𝙾𝚁𝙺 📡", "https://t.me/ignite_Xnetwork")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
