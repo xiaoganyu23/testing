cc33e1a01f86955490d494918487dc7bd67c5ab8
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-05-18 06:45:54 +0530

    Update alive.py

--- zerotwobot/modules/alive.py
+++ zerotwobot/modules/alive.py
@@ -9,16 +9,16 @@ from zerotwobot.events import register
 from zerotwobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7e678b34fb62a00dd23a0.jpg"
+PHOTO = "https://telegra.ph/file/38513d13b5f772f2a36bd.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Naruto Uzumaki.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Officer Jenny.** \n\n"
   TEXT += "❍ **I'm Working Properly** \n\n"
   TEXT += f"❍ **My Master : [Rin Okumura](https://t.me/Redeye_Ghoul)** \n\n"
   TEXT += f"❍ **Library Version :** `{telever}` \n\n"
   TEXT += f"❍ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"❍ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ✉️", "https://t.me/naruto_robot_support"), Button.url("𝙽𝙴𝚃𝚆𝙾𝚁𝙺 📡", "https://t.me/ignite_Xnetwork")]]
+  BUTTON = [[Button.url("𝚂𝚄𝙿𝙿𝙾RT", "https://t.me/BOTPEROSUPPORT"), Button.url("OWNER", "https://t.me/SILVER_KING")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
